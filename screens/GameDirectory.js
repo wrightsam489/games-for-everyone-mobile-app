b4c0f5944496760c6de7eaaebf350f109a2e955e
wrightsam489 <wrightsam489@gmail.com>
2025-09-16 19:07:54 -0500

    Updated project to start making calls to custom api

--- screens/GameDirectory.js
+++ screens/GameDirectory.js
@@ -1,4 +1,5 @@
-import { Dimensions, StyleSheet, FlatList, SafeAreaView, View, Image, Pressable, SectionList } from 'react-native'
+import { useEffect, useState } from 'react';
+import { Dimensions, StyleSheet, FlatList, SafeAreaView, View, Image, Pressable, SectionList, Platform } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
 
 import { useTheme } from '../contexts/ThemeContext'
@@ -6,10 +7,10 @@ import { Heading, Subheading } from '../components/Texts'
 import { SearchBar } from '../components/TextFields';
 
 const { width } = Dimensions.get('window');
-const CONTAINER_WIDTH = width * .8;
+const CONTAINER_WIDTH = width * .5;
 const CONTAINER_SPACING = (width - CONTAINER_WIDTH) / 2
-const IMAGE_WIDTH = CONTAINER_WIDTH * .95;
-const IMAGE_HEIGHT = IMAGE_WIDTH * 2 / 3;
+const IMAGE_WIDTH = CONTAINER_WIDTH * .9;
+const IMAGE_HEIGHT = IMAGE_WIDTH;
 
 const DATA = [
   {
@@ -59,6 +60,26 @@ export default function GameDirectory() {
   const styles = makeStylesSheet(theme.colors)
   const navigation = useNavigation();
 
+  const API_BASE_URL = Platform.OS === 'android' ? 'http://10.0.2.2:5000' : 'http://localhost:5000';
+
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+
+  const fetchData = async () => {
+    await fetch(`${API_BASE_URL}/games/genre/2`)
+      .then(response => response.json())
+      .then(json => {
+        setGames(json)
+      })
+      .catch(err => {
+        console.error('Fetch Error:', err);
+      })
+      .finally()
+  };
+
   function GameCard({ item }) {
     return (
       <View style={styles.item}>
@@ -71,9 +92,9 @@ export default function GameDirectory() {
         >
           <Image 
             style={styles.image}
-            source={item.cover}
+            source={{ uri: item.cover}}
           />
-          <Subheading style={{padding: 5}}>Hollow Knight</Subheading>
+          <Subheading style={{padding: 5, numberOfLines: 0, ellipsizeMode: 'clip'}}>{item.title}</Subheading>
         </Pressable>
       </View>
     )
@@ -100,7 +121,7 @@ export default function GameDirectory() {
       <SearchBar style={{ margin: 15 }} />
       <SectionList 
         contentContainerStyle={{ paddingBottom: 10, rowGap: 5 }}
-        sections={DATA}
+        sections={games}
         keyExtractor={(item, index) => item.id + index}
         renderItem={() => {}}
         renderSectionHeader={({section: {genre, data}}) => (
