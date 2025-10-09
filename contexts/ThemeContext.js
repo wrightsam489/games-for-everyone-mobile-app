fb72c278402d8c7b52f8b23a815d741e084ddf1b
wrightsam489 <wrightsam489@gmail.com>
2025-10-09 17:29:59 -0500

    Updated light mode theming

--- contexts/ThemeContext.js
+++ contexts/ThemeContext.js
@@ -1,54 +1,56 @@
 import { createContext, useEffect, useContext, useState, useMemo } from "react";
-import { useColorScheme } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useColorScheme } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import FontStyles from "../styles/FontStyles";
 
 const lightTheme = {
   dark: false,
   colors: {
-    primary: '#00A74F',
-    secondary: '#0072CE',
-    destructive: '#D32F2F',
-    background: '#F8F5E6',
-    card: '#ffffff',
-    text: '#212121',
-    border: '#212121',
-    notification: '#ffffff',
-    disabled: '#7A7A7A',
-    highlight: '#E91E63',
-    textField: '#ffffff',
+    primary: "#00A74F",
+    secondary: "#0072CE",
+    destructive: "#D32F2F",
+    background: "#ffffff",
+    navigation: "#ebebeb",
+    card: "#ebebeb",
+    text: "#212121",
+    border: "#212121",
+    notification: "#ebebeb",
+    disabled: "#7A7A7A",
+    highlight: "#E91E63",
+    textField: "#ebebeb",
   },
   fonts: {
-      heavy: FontStyles.heavy,
-      medium: FontStyles.medium,
-      bold: FontStyles.bold,
-      regular: FontStyles.regular,
-  }
-}
+    heavy: FontStyles.heavy,
+    medium: FontStyles.medium,
+    bold: FontStyles.bold,
+    regular: FontStyles.regular,
+  },
+};
 
 const darkTheme = {
   dark: true,
   colors: {
-    primary: '#39B54A',
-    secondary: '#0070D1',
-    destructive: '#E74C3C',
-    background: '#121212',
-    card: '#3d3d3dff',
-    text: '#F5F5F5',
-    border: '#F5F5F5',
-    notification: '#3d3d3dff',
-    disabled: '#7A7A7A',
-    highlight: '#E91E63',
-    textField: '#3d3d3dff',
+    primary: "#39B54A",
+    secondary: "#0070D1",
+    destructive: "#E74C3C",
+    background: "#121212",
+    navigation: "#3d3d3dff",
+    card: "#3d3d3dff",
+    text: "#F5F5F5",
+    border: "#F5F5F5",
+    notification: "#3d3d3dff",
+    disabled: "#7A7A7A",
+    highlight: "#E91E63",
+    textField: "#3d3d3dff",
   },
   fonts: {
-      heavy: FontStyles.heavy,
-      medium: FontStyles.medium,
-      bold: FontStyles.bold,
-      regular: FontStyles.regular,
-  }
-}
+    heavy: FontStyles.heavy,
+    medium: FontStyles.medium,
+    bold: FontStyles.bold,
+    regular: FontStyles.regular,
+  },
+};
 
 export const ThemeContext = createContext(lightTheme);
 
@@ -56,7 +58,7 @@ export const ThemeProvider = ({ children }) => {
   const systemScheme = useColorScheme();
   const [mode, setMode] = useState(null);
 
-    useEffect(() => {
+  useEffect(() => {
     (async () => {
       try {
         const stored = await AsyncStorage.getItem("mode");
@@ -87,7 +89,7 @@ export const ThemeProvider = ({ children }) => {
   const theme = useMemo(() => {
     if (mode === "light") return lightTheme;
     if (mode === "dark") return darkTheme;
-    setMode(systemScheme)
+    setMode(systemScheme);
     return systemScheme === "dark" ? darkTheme : lightTheme;
   }, [mode, systemScheme]);
 
--- screens/GameDetails.js
+++ screens/GameDetails.js
@@ -63,31 +63,17 @@ export default function GameDetails({ route }) {
       <Button onPress={() => fetchData()} title="Reload" />
       {game ? (
         <ScrollView>
-          <Image
-            style={{ width: IMAGE_WIDTH, height: IMAGE_HEIGHT }}
-            source={{ uri: GameDetails.cover }}
-          />
+          {game.cover && (
+            <Image
+              style={{
+                width: IMAGE_WIDTH,
+                height: IMAGE_HEIGHT,
+              }}
+              source={{ uri: game.cover }}
+            />
+          )}
           <View style={{ paddingHorizontal: 15, marginTop: 15 }}>
-            <View
-              style={{ flexDirection: "row", justifyContent: "space-between" }}
-            >
-              <Title>{game.title}</Title>
-              <View style={{ flexDirection: "row" }}>
-                <IconButton
-                  onPress={() => rateGame("good")}
-                  iconName={rating == "good" ? "thumbs-up" : "thumbs-o-up"}
-                  size={35}
-                  color={"green"}
-                />
-                <IconButton
-                  onPress={() => rateGame("bad")}
-                  iconName={rating == "bad" ? "thumbs-down" : "thumbs-o-down"}
-                  size={35}
-                  color={"red"}
-                />
-              </View>
-            </View>
-
+            <Title>{game.title}</Title>
             <Heading style={{ paddingTop: 10, paddingBottom: 5 }}>
               Description
             </Heading>
@@ -120,44 +106,6 @@ export default function GameDetails({ route }) {
                   </>
                 );
               })}
-
-            <Heading style={{ paddingTop: 10, paddingBottom: 5 }}>
-              Status
-            </Heading>
-            <View
-              style={{ flexDirection: "row", justifyContent: "space-evenly" }}
-            >
-              <SecondaryButton
-                style={{ paddingHorizontal: 10, borderRadius: 0 }}
-                title={"Playing"}
-              />
-              <SecondaryButton
-                style={{ paddingHorizontal: 10, borderRadius: 0 }}
-                title={"Plan to play"}
-              />
-              <SecondaryButton
-                style={{ paddingHorizontal: 10, borderRadius: 0 }}
-                title={"On hold"}
-              />
-            </View>
-
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-evenly",
-                marginTop: 15,
-                paddingBottom: 25,
-              }}
-            >
-              <SecondaryButton
-                style={{ paddingHorizontal: 10, borderRadius: 0 }}
-                title={"Dropped"}
-              />
-              <PrimaryButton
-                style={{ paddingHorizontal: 10, borderRadius: 0 }}
-                title={"Completed"}
-              />
-            </View>
           </View>
         </ScrollView>
       ) : (
